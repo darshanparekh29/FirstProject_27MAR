aceef8fa1c4e6c9de8d46410e2d3e40bad322750
darshanparekh29 <darshan.parekh.2024@gmail.com>
2024-03-28 11:25:24 +0530

    add paggination with Exam and Attandance Grid

--- router.js
+++ router.js
@@ -138,48 +138,48 @@ function stuructureSQL(sql) {
     sql = `SELECT * FROM student_master WHERE first_name ${sql1} AND last_name ${sql2} AND gender ${sql3} AND email ${sql4} AND city ${sql5} AND DOB ${sql6}`;
     return sql;
 }
-function StoreQuery(sql){
-    fs.writeFile("file.txt",(sql),
-        err=>{
-            if(err) throw err;
+function StoreQuery(sql) {
+    fs.writeFile("file.txt", (sql),
+        err => {
+            if (err) throw err;
             console.log("query in Json Inserted!");
         });
 }
 
-function checkResults(dataLength){
-    if(dataLength==0){
+function checkResults(dataLength) {
+    if (dataLength == 0) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
-function checkPages(dataLength){
+function checkPages(dataLength) {
     var a;
-    if(dataLength%15==0){
-        a=dataLength/15;   
+    if (dataLength % 15 == 0) {
+        a = dataLength / 15;
         return a;
     }
-    else{
-        a=(dataLength/15)+1;
+    else {
+        a = (dataLength / 15) + 1;
         return Math.floor(a);
     }
 }
-function lastPageValues(dataLength){
+function lastPageValues(dataLength) {
     var b;
-    if(dataLength%15==0){
-        b=15;
+    if (dataLength % 15 == 0) {
+        b = 15;
         return b;
     }
-    else{
-        a=(dataLength/15)+1;
-        b=5;
+    else {
+        a = (dataLength / 15) + 1;
+        b = 5;
         return b;
     }
 }
 
 router.get("/task", (req, res) => {
-    
+
 
     var taskName = req.query.taskName;
     var filter = req.query.filter || undefined;
@@ -229,16 +229,16 @@ router.get("/task", (req, res) => {
         // console.log(orderBy);
         var order = req.query.order || "ASC";
         if (secondP == 1) {
-          //  console.log("Hello!2");
+            //  console.log("Hello!2");
             try {
                 var sql1 = req.query.query;
-               // console.log("sql1 "+sql1);
+                // console.log("sql1 "+sql1);
                 var parms = req.query.parms || 2;
                 if (parms == 1) {
 
                     var page = req.query.page || 1;
                     var offset = (page - 1) * 15;
-                    console.log("sql1 "+sql1);
+                    console.log("sql1 " + sql1);
                     con.con.query(sql1, function (err, result1) {
                         if (err) {
                             // res.status(404).send("You enterd Wrong Query!");
@@ -261,7 +261,7 @@ router.get("/task", (req, res) => {
                     });
                 } else {
                     //  console.log("Heljnxflo");
-                   
+
                     StoreQuery(sql1);
                     var page = req.query.page || 1;
                     var offset = (page - 1) * 15;
@@ -304,7 +304,7 @@ router.get("/task", (req, res) => {
 
 
         if (secondP == 1) {
-          //  console.log("HelloSecondP!");
+            //  console.log("HelloSecondP!");
             try {
 
                 var sql1 = req.query.query;
@@ -313,7 +313,7 @@ router.get("/task", (req, res) => {
 
                     var page = req.query.page || 1;
                     var offset = (page - 1) * 15;
-                  
+
                     con.con.query(sql1, function (err, result1) {
                         if (err) {
                             // res.status(404).send("You enterd Wrong Query!");
@@ -340,9 +340,9 @@ router.get("/task", (req, res) => {
 
                     });
                 } else {
-                   
+
                     StoreQuery(sql1);
-                    console.log("sql1 "+sql1);
+                    console.log("sql1 " + sql1);
                     var page = req.query.page || 1;
                     var offset = (page - 1) * 15;
                     //   var orderBy = req.query.orderBy || "exam_id";
@@ -377,8 +377,8 @@ router.get("/task", (req, res) => {
             res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithSearch/index.ejs");
         }
     }
-  
-    else if(taskName=="startFilter"){
+
+    else if (taskName == "startFilter") {
 
         //console.log("Hello!");
         var query = req.query.query;
@@ -387,130 +387,253 @@ router.get("/task", (req, res) => {
         var dataa3 = req.query.data3;
         var dataa4 = req.query.data4;
         var dataa5 = req.query.data5;
-        var orderBy = req.query.orderBy ||  'exam_id';
-       // console.log(orderBy)
+        var orderBy = req.query.orderBy || 'exam_id';
+        // console.log(orderBy)
         var order = req.query.order || "ASC";
-    
-        
+
+
         // var sql = `${query} WHERE stu_id LIKE '%${dataa1}%' ${dataa5} subject LIKE '%${dataa2}%' ${dataa5} exam_type LIKE '%${dataa3}%' ${dataa5} practical_marks LIKE '%${dataa4}%'`;
         var sql = `${query} WHERE stu_id LIKE '${dataa1}' ${dataa5} subject LIKE '${dataa2}' ${dataa5} exam_type LIKE '${dataa3}' ${dataa5} practical_marks LIKE '${dataa4}'`;
-        var sql1=`${sql};`;
-       
-              
-            var page=req.query.page || 1;
-            var offset=(page-1)*15;
-            con.con.query(sql1,function(err,result1){
-                if(err){
+        var sql1 = `${sql};`;
+
+
+        var page = req.query.page || 1;
+        var offset = (page - 1) * 15;
+        con.con.query(sql1, function (err, result1) {
+            if (err) {
+                // res.status(404).send("You enterd Wrong Query!");
+                res.send("You enterd wrong data" + err);
+            };
+            var dataLength = result1.length;
+            var checkResult = checkResults(dataLength);
+            // if(checkResult==true){
+
+            // }
+            var requirePages = checkPages(dataLength);
+            totalPages = 15;
+            if (page == requirePages) {
+                totalPages = lastPageValues(dataLength)
+            }
+            //fs.readFile("file.txt", function(err, data) {
+
+            sql2 = sql + ` ORDER BY ${orderBy} ${order} LIMIT ${totalPages} OFFSET ${offset};`;
+
+            //console.log("sql2 "+sql2);
+            con.con.query(sql2, function (err, result, fields1) {
+                if (err) {
                     // res.status(404).send("You enterd Wrong Query!");
-                    res.send("You enterd wrong data"+err);
+                    res.send("You enterd wrong data" + err);
                 };
-                var dataLength=result1.length;
-                var checkResult = checkResults(dataLength);
-                // if(checkResult==true){
-                    
-                // }
-                var requirePages= checkPages(dataLength);
-                totalPages=15;
-                if(page==requirePages){
-                    totalPages=lastPageValues(dataLength)
+                //  console.log(result);
+                res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithSearch/filterIndex.ejs", { data1: result, data2: fields1, page, requirePages, query, dataa1, dataa2, dataa3, dataa4, dataa5, checkResult, order, orderBy });
+            });
+            //  });            
+        });
+    }
+    else if (taskName == "PaginationWithDeliSearch") {
+        var page = req.query.page || 1;
+        var offset = (page - 1) * 15;
+
+
+
+        sql = "select student_id,first_name,last_name,father_name,mother_name,gender,email,city,p_number,em_number from student_master";
+        StoreQuery(sql);
+
+        sql2 = sql + ";";
+        con.con.query(sql2, function (err, result1, fields1) {
+            if (err) {
+                res.send("Sql query is not proper!  ERR: " + err);
+            }
+            else {
+                var dataLength = result1.length;
+                var requirePages = checkPages(dataLength);
+                totalPages = 15;
+                if (page == requirePages) {
+                    totalPages = lastPageValues(dataLength)
                 }
-                //fs.readFile("file.txt", function(err, data) {
-                        
-                    sql2=sql+` ORDER BY ${orderBy} ${order} LIMIT ${totalPages} OFFSET ${offset};`;
-                    
-                    //console.log("sql2 "+sql2);
-                    con.con.query(sql2,function(err,result,fields1){
-                        if(err){
-                            // res.status(404).send("You enterd Wrong Query!");
-                            res.send("You enterd wrong data"+err);
-                        };
-                      //  console.log(result);
-                        res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithSearch/filterIndex.ejs",{data1:result,data2:fields1,page,requirePages,query,dataa1,dataa2,dataa3,dataa4,dataa5,checkResult,order,orderBy});                    
-                    });            
-                //  });            
+
+                fs.readFile("file.txt", function (err, data) {
+
+                    sql2 = data.toString() + ` LIMIT ${totalPages} OFFSET ${offset};`;
+
+
+                    con.con.query(sql2, function (err, result, fields) {
+                        // if(err)  throw err;
+                        res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithDeliSearch/index.ejs", { data1: result, data2: fields, page, requirePages });
+                    });
+                });
+            }
+        });
+    }
+    else if (taskName == "PaginationWithExamAndAttandanceGrid") {
+        var secondTaskName = req.query.secondTaskName;
+        //console.log(secondTaskName)
+        if (secondTaskName == "taskHome") {
+            var array1 = [];
+            var array2 = [];
+            var page = req.query.page || 1;
+            var offset = (page - 1) * 10;
+            var sql = `SELECT student_master.student_id,student_master.first_name,
+    SUM(
+    case 
+    when exam_type="Prelims" then theory_marks
+    else 0
+    end
+    )as prelim_theroy_marks,
+    SUM(
+    case 
+    when exam_type="Prelims" then practical_marks
+    else 0
+    end
+    )as prelim_practical_marks,
+    SUM(
+    CASE
+    WHEN exam_type="Terminnal" then theory_marks
+    else 0
+    end
+    )as Terminal_Theory_marks,
+    SUM(
+    CASE
+    WHEN exam_type="Terminnal" then practical_marks
+    else 0
+    end
+    )as Terminal_Practical_marks,
+    SUM(
+    CASE
+    WHEN exam_type="Final" then theory_marks
+    else 0
+    end
+    )as Final_Theroy_marks,
+    SUM(
+    CASE
+    WHEN exam_type="Final" then practical_marks
+    else 0
+    end
+    )as Final_Practical_marks,
+    SUM(theory_marks+practical_marks) as Total_Marks
+    FROM exam_master
+    LEFT JOIN student_master 
+    ON exam_master.stu_id=student_master.student_id
+    GROUP BY exam_master.stu_id LIMIT 10 OFFSET ${offset};`;
+            con.con.query(sql, function (err, result, field) {
+                if (err) throw err;
+                res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithExamAndAttandance/index.ejs", { data: result, page });
             });
         }
-        else if(taskName=="PaginationWithDeliSearch"){
-            var page = req.query.page || 1;
-            var offset = (page - 1) * 15;
+    }
+    if (secondTaskName == "taskReport"){
+        var Value=req.query.student;
+    sql=`SELECT subject,
+    SUM(
+        case 
+        when exam_type="Prelims" then theory_marks
+        else 0
+        end
+        )as prelim_theroy_marks,
+        SUM(
+        case 
+        when exam_type="Prelims" then practical_marks
+        else 0
+        end
+        )as prelim_practical_marks,
+        SUM(
+        CASE
+        WHEN exam_type="Terminnal" then theory_marks
+        else 0
+        end
+        )as Terminal_Theory_marks,
+        SUM(
+        CASE
+        WHEN exam_type="Terminnal" then practical_marks
+        else 0
+        end
+        )as Terminal_Practical_marks,
+        SUM(
+        CASE
+        WHEN exam_type="Final" then theory_marks
+        else 0
+        end
+        )as Final_Theroy_marks,
+        SUM(
+        CASE
+        WHEN exam_type="Final" then practical_marks
+        else 0
+        end
+        )as Final_Practical_marks,
+        SUM(theory_marks+practical_marks) as Total_Marks
+    FROM student_27FEB_DB.exam_master WHERE stu_id=${Value} GROUP BY subject;`;
+    con.con.query(sql,function(err,result,field){
+        if(err) throw err;
+        res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithExamAndAttandance/reportCard.ejs",{data:result});
+    });
+    }
+    if(secondTaskName=="taskAtt"){
         
-        
-         
-                sql = "select student_id,first_name,last_name,father_name,mother_name,gender,email,city,p_number,em_number from student_master";
-                StoreQuery(sql);
-        
-                sql2=sql+";";
-                con.con.query(sql2, function (err, result1, fields1) {
-                    if (err) {
-                        res.send("Sql query is not proper!  ERR: " + err);
-                    }
-                    else {
-                        var dataLength = result1.length;
-                        var requirePages = checkPages(dataLength);
-                        totalPages = 15;
-                        if (page == requirePages) {
-                            totalPages = lastPageValues(dataLength)
-                        }
-            
-                        fs.readFile("file.txt", function(err, data) {
-                                        
-                            sql2=data.toString()+` LIMIT ${totalPages} OFFSET ${offset};`;
-                        
-                        
-                        con.con.query(sql2,function(err,result,fields){
-                            // if(err)  throw err;
-                            res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithDeliSearch/index.ejs",{data1:result,data2:fields,page,requirePages});                    
-                        });            
-                      });   
-                    }
-                });
-        }
+        var Value=req.query.student;
+
+    var month = req.query.month || "JAN 2024";
+    var totalDaysinMonth =0;
+    if(month=="FEB 2024"){
+        totalDaysinMonth=29;
+    }
+    else{
+        totalDaysinMonth=31;
+    }
+    var sql = `SELECT a.student_id,b.first_name,b.last_name,count(a.isPresent) as present_days,ROUND(COUNT(b.student_id)/.${totalDaysinMonth},2) AS "PERCENTAGE"
+    FROM attandance_master as a
+    INNER JOIN student_master as b
+    ON a.student_id = b.student_id WHERE att_month="${month}" AND isPresent=1 AND a.student_id=${Value} GROUP BY a.student_id`;
+    con.con.query(sql,function (err,result,fields){
+        if (err) throw err;
+        res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithExamAndAttandance/student_attandance.ejs",{data:result,month});
+    })
+    }
 })
 
-router.post("/taskWithSearch",(req,res)=>{
-    var page=req.query.page || 1;
-    order="ASC";
-    
+router.post("/taskWithSearch", (req, res) => {
+    var page = req.query.page || 1;
+    order = "ASC";
+
     var varId = req.body["enterData"];
-   // const myArray = varId.split(",");
-    
-    fs.readFile("file.txt", function(err, data) {                           
-        sql1=data.toString()+`;`;
-        con.con.query(sql1,function(err,result,fields){    
-            if(err){
+    // const myArray = varId.split(",");
+
+    fs.readFile("file.txt", function (err, data) {
+        sql1 = data.toString() + `;`;
+        con.con.query(sql1, function (err, result, fields) {
+            if (err) {
                 // res.status(404).send("You enterd Wrong Query!");
-                res.send("You enterd wrong data"+err);
+                res.send("You enterd wrong data" + err);
             };
-          
 
-        sql2=data.toString()+` WHERE ${fields[0].name} IN (${varId});`;
-        
-        orderBy=`${fields[0].name}`;
-        con.con.query(sql2,function(err,result1,fields1){    
 
-            
-            var requirePages= 1;
+            sql2 = data.toString() + ` WHERE ${fields[0].name} IN (${varId});`;
 
-            if(err){
-                // res.status(404).send("You enterd Wrong Query!");
-                res.send("You enterd wrong data"+err);
-            };
-            
-            res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithSearch/detailList.ejs",{data1:result1,data2:fields1,page,requirePages,sql2,order,orderBy});         
-        });   
-             
-    });            
-  });     
+            orderBy = `${fields[0].name}`;
+            con.con.query(sql2, function (err, result1, fields1) {
+
+
+                var requirePages = 1;
+
+                if (err) {
+                    // res.status(404).send("You enterd Wrong Query!");
+                    res.send("You enterd wrong data" + err);
+                };
+
+                res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithSearch/detailList.ejs", { data1: result1, data2: fields1, page, requirePages, sql2, order, orderBy });
+            });
+
+        });
+    });
 })
-router.post("/DeliSearch",(req,res)=>{
+router.post("/DeliSearch", (req, res) => {
     var page = req.query.page || 1;
     var offset = (page - 1) * 15;
 
     var sql = stuructureSQL(req.body.searchResult);
     StoreQuery(sql);
-   // console.log("sql"+sql);
-    
-    var sql2=sql+";";
+    // console.log("sql"+sql);
+
+    var sql2 = sql + ";";
     con.con.query(sql2, function (err, result1, fields1) {
         if (err) {
             res.send(err);
@@ -523,16 +646,16 @@ router.post("/DeliSearch",(req,res)=>{
                 totalPages = lastPageValues(dataLength)
             }
 
-            fs.readFile("file.txt", function(err, data2) {
-                            
-                sql2=data2.toString()+` LIMIT ${totalPages} OFFSET ${offset};`;
-            
-            
-            con.con.query(sql2,function(err,result,fields){
-                // if(err)  throw err;
-                res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithDeliSearch/index.ejs",{data1:result,data2:fields,page,requirePages});                    
-            });            
-          });   
+            fs.readFile("file.txt", function (err, data2) {
+
+                sql2 = data2.toString() + ` LIMIT ${totalPages} OFFSET ${offset};`;
+
+
+                con.con.query(sql2, function (err, result, fields) {
+                    // if(err)  throw err;
+                    res.render("/home/darshan-parekh/Desktop/FirstProject/FirstProject_27MAR/views/pagginationWithDeliSearch/index.ejs", { data1: result, data2: fields, page, requirePages });
+                });
+            });
         }
     });
 })
